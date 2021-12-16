4afe989f059fc370c0d12946b5da5b3d9cbf2d4c
Your Name <you@example.com>
2021-12-16 23:03:23 +0100

    added token usage, read from the file

--- src/github_api.rs
+++ src/github_api.rs
@@ -1,22 +1,42 @@
 use reqwest;
 use reqwest::header::USER_AGENT;
+use std::fs;
 use std::io::Read;
 
-pub fn get_repos() -> Result<(), reqwest::Error> {
-    let client = reqwest::blocking::Client::new();
+pub struct GithubApi {
+    username: String,
+    token: String,
+}
 
-    let mut res = client
-        .get("https://api.github.com/zen")
-        .header(USER_AGENT, "bus_factor")
-        .send()?;
+impl GithubApi {
+    pub fn new(username: &str, token: &str) -> Self {
+        Self {
+            username: username.to_string(),
+            token: token.to_string(),
+        }
+    }
 
-    let mut body = String::new();
+    // TODO: do a test
+    /// Test token, this endpoint requires basic auth
+    pub fn requires_token(&self) -> Result<(), reqwest::Error> {
+        let client = reqwest::blocking::Client::new();
 
-    res.read_to_string(&mut body);
+        let endpoint = format!("https://api.github.com/users/{}/hovercard", self.username);
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
+        let mut res = client
+            // .get("https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc&per_page=5")
+            .get(endpoint)
+            .header(USER_AGENT, "bus_factor")
+            .basic_auth("szymek156", Some(&self.token))
+            .send()?;
 
-    Ok(())
+        let mut body = String::new();
+
+        res.read_to_string(&mut body);
+
+        println!("Status: {}", res.status());
+        println!("Body:\n{}", body);
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod github_api;
 
+use std::fs;
+
+use github_api::GithubApi;
 use structopt::StructOpt;
-use  github_api::get_repos;
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "bus_factor",
-    about = "Command to gather bus factor statistics from gtihub repos.",
-
+    about = "Command to gather bus factor statistics from gtihub repos."
 )]
 struct Opt {
     /// Programming language name
@@ -16,9 +17,30 @@ struct Opt {
     /// Number of projects to consider
     #[structopt(short, long)]
     project_count: u32,
+
+    /// Filepath for token
+    #[structopt(short, long, default_value = "./.token")]
+    token_path: String,
+
+    // TODO: username is needed?
+    #[structopt(short, long, default_value = "szymek156")]
+    username: String,
+}
+
+// TODO: better to pass str or String?
+pub fn get_token(filepath: &str) -> String {
+    // TODO: why there is a newline?
+    let mut contents = fs::read_to_string(filepath).expect("Something went wrong reading the file");
+
+    contents
 }
 
 fn main() {
-    // let opt = Opt::from_args();
-    get_repos();
+    let opt = Opt::from_args();
+
+    let token = get_token(&opt.token_path);
+
+    let api = GithubApi::new(&opt.username, &token);
+
+    api.requires_token();
 }
