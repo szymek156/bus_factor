ef1221a5d93d05afdf6f9ef05872b8a2f3b8c66b
Your Name <you@example.com>
2021-12-17 00:19:37 +0100

    parsing result to json

--- src/github_api.rs
+++ src/github_api.rs
@@ -1,8 +1,16 @@
 use reqwest;
 use reqwest::header::USER_AGENT;
+use serde_json::Value;
 use std::fs;
 use std::io::Read;
 
+//TODO: use enum where applicable
+pub struct Query<'a>{
+    pub keyword: &'a str,
+    pub language: &'a str,
+    pub sort: &'a str,
+    pub order: &'a str
+}
 pub struct GithubApi {
     username: String,
     token: String,
@@ -36,6 +44,39 @@ impl GithubApi {
 
         println!("Status: {}", res.status());
         println!("Body:\n{}", body);
+
+        Ok(())
+    }
+
+    // TODO: use anyhow, or something for err handling
+    pub fn get_repos(&self, query: &Query) -> Result<(), reqwest::Error> {
+
+
+        let client = reqwest::blocking::Client::new();
+
+        // q=tetris+language:assembly&sort=stars&order=desc
+
+        let endpoint =
+            format!("https://api.github.com/search/repositories?q={keyword}+language:{language}&sort={sort}&order={order}&per_page=1",
+             keyword=query.keyword, language=query.language, sort=query.sort,order=query.order);
+
+        let mut res = client
+            // .get("https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc&per_page=5")
+            .get(endpoint)
+            .header(USER_AGENT, "bus_factor")
+            .basic_auth("szymek156", Some(&self.token))
+            .send()?;
+
+        let mut body = String::new();
+
+        res.read_to_string(&mut body);
+
+        println!("Status: {}", res.status());
+
+        // TODO: reqwest error, and serde error, how to propagate both?
+        let body: Value = serde_json::from_str(&body).unwrap();
+
+        println!("Body:\n{}", body["items"][0]["name"]);
 
         Ok(())
     }
