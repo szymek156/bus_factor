00b943e014f89d0ac721261701dd21d4de2d2c78
szymek156 <szymon.zimnowoda@gmail.com>
2021-12-19 02:23:12 +0100

    added request error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[macro_use]
+extern crate log;
+
+mod api_errors;
 mod github_api;
 
 use std::fs;
@@ -41,19 +45,70 @@ pub fn get_token(filepath: &str) -> String {
 // TODO: pretty formatting of the result
 // TODO: read about bearer auth
 // TODO: get rid of username
+// TODO: return errs with context
 // TODO: test the cli
-
+// TODO: 0 has a special meaning, returns all (30 - default per page)
+// cargo run -- --language rust --project-count 0
 fn main() {
+    env_logger::init();
+
     let opt = Opt::from_args();
 
     let token = get_token(&opt.token_path);
 
     let api = GithubApi::new(&opt.username, &token);
 
-    // api.requires_token();
     api.get_projects(&Query {
         language: &opt.language,
         count: opt.project_count,
     })
     .unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{fs, path::PathBuf};
+
+    use crate::api_errors::ResponseError;
+
+    use super::*;
+
+    fn load_token() -> String {
+        let mut filepath = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        filepath.push(".token");
+
+        let token = fs::read_to_string(filepath).expect("Something went wrong reading the file");
+
+        token
+    }
+
+    #[test]
+    /// Checks if usage and value of the token are valid
+    /// Test requires token to be in root/.token
+    fn simple_call_works() {
+        let token = load_token();
+        let api = GithubApi::new("szymek156", &token);
+
+        api.get_projects(&Query {
+            language: "rust",
+            count: 1,
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn api_fails_message_is_propagated() {
+        let token = load_token();
+        let api = GithubApi::new("szymek156", &token);
+
+        // Linux is C project, with too many contributions to show, api will fail
+        // assert_eq!(
+        //     ResponseError,
+        //     *api.get_projects(&Query {
+        //         language: "C",
+        //         count: 1,
+        //     })
+        //     .unwrap_err()
+        // );
+    }
 }
